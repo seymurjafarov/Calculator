3113031cfc82c914b2e3de7b7a8000871435a46a
seymurjafarov <jafar.seymur@gmail.com>
2022-05-25 17:45:00 +0400

    "Calculyator duzeltdim"

---
+++ Calculate.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Calculate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("please enter figure");
+        double x = sc.nextDouble();
+
+        System.out.println("please enter figure");
+        double y = sc.nextDouble();
+
+        System.out.println("please enter operation");
+        char o = sc.next().charAt(0);
+
+        double r = 0;
+
+        if (o == '+') {
+            r = Utils.Topla(x,y);
+        } else if (o == '-') {
+            r = Utils.Cixma(x,y);
+        } else if (o == '*') {
+            r = Utils.Vurma(x,y);
+        } else if (o == '/') {
+            r = Utils.Bolme(x,y);
+        }else {
+            System.out.println("operation not found");
+        }
+        System.out.println(r);
+    }
+}
---
+++ Utils.java
@@ -0,0 +1,19 @@
+public class Utils {
+
+    public static double Topla(double a,double b) {
+        return  a + b;
+    }
+
+    public static double Cixma(double a,double b) {
+        return  a - b;
+    }
+
+    public static double Vurma(double a,double b) {
+        return  a * b;
+    }
+
+    public static double Bolme(double a,double b) {
+        return  a / b;
+    }
+
+}
